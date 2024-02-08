8fc8232f51beae7e8531447a8590a479064609a1
AidanSimpson <aidansimpson0@gmail.com>
2024-02-08 02:38:51 -0500

    initial file uploads for new website

---
+++ README.md
@@ -0,0 +1,17 @@
+# websitesimpson
+
+An Application designed and coded by Aidan Simpson.
+
+Hello!
+Take a look around, and please feel free to reach out!
+
+To view this project locally:
+
+- Set this project up with DDEV.
+- Set this project up with Lando.
+- If using Visual Studio Code, install and run the `Live Server` extension, by Ritwick Dey.
+- Utilizing Wamp / MAMP and localhost.
+
+This project contains HTML, CSS / SCSS, and JavaScript.
+
+Cheers
---
+++ js/index.js
@@ -0,0 +1,33 @@
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const media = window.matchMedia('(width < 40em)');
+const topNavMenu = document.querySelector('.topnav__menu');
+// const main = document.querySelector('main');
+
+function openMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'true');
+  topNavMenu.removeAttribute('style');
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'false');
+  btnOpen.focus();
+}
+
+function setupTopNav(e) {
+  if (e.matches) {
+    topNavMenu.style.transition = 'none';
+  } else {
+    closeMobileMenu();
+  }
+}
+
+setupTopNav(media);
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+media.addEventListener('change', function (e) {
+  setupTopNav(e);
+});
